fc33786678a363ba91ea1542fa00b6f72517f5be
reytor9010 <reytor9010@gmail.com>
2021-05-20 16:50:16 -0400

    Upload FizzBuzz project

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+
+namespace FizzBuzz
+{
+	public class FizzBuzz
+	{
+		#region Public Methods
+
+		/// <summary>
+		/// Get the FizzBuzz message for all the values in an inclusive range [<paramref name="from"/>, <paramref name="to"/>] separated by a new line.
+		/// </summary>
+		/// <param name="from"></param>
+		/// <param name="to"></param>
+		public string GetFizzBuzzForRange(int from = 1, int to = 100)
+		{
+			if (from > to)
+			{
+				throw new ArgumentException("Invalid range");
+			}
+
+			StringBuilder messageStringBuilder = new StringBuilder();
+
+			for (int i = from; i <= to; i++)
+			{
+				messageStringBuilder.Append(GetFizzBuzzForValue(i));
+				messageStringBuilder.Append(i < to ? "\n" : "");
+			}
+
+			return messageStringBuilder.ToString();
+		}
+
+		#endregion
+
+		#region Private Methods
+
+		/// <summary>
+		/// Get the FizzBuzz message for a single value
+		/// </summary>
+		/// <param name="value"></param>
+		private string GetFizzBuzzForValue(int value) => (value % 3, value % 5) switch
+		{
+			(0, 0) => "FizzBuzz",
+			(0, _) => "Fizz",
+			(_, 0) => "Buzz",
+			_ => value.ToString()
+		};
+
+		#endregion
+	}
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			FizzBuzz fizzBuzz = new FizzBuzz();
+			string fizzbuzzMessage;
+			int from = 0;
+			int to = 0;
+
+			while (true)
+			{
+				//Program Description
+				Console.ForegroundColor = ConsoleColor.Green;
+				Console.WriteLine("\tFIZZ - BUZZ PROGRAM");
+				Console.WriteLine();
+				Console.WriteLine("For each value in an inclusive range [from, to] prints: ");
+				Console.WriteLine("  - \"Fizz\" if the value is divisible by 3");
+				Console.WriteLine("  - \"Buzz\" if the value is divisible by 5");
+				Console.WriteLine("  - \"FizzBuzz\" if the value is divisible by 3 and by 5");
+				Console.WriteLine("  - Otherwise prints the value itself");
+				Console.WriteLine();
+				Console.WriteLine("* Floating point range values are allowed, but it will be rounded to the nearest int * ");
+				Console.WriteLine("* Any invalid input will tranlate into a value of 0 * ");
+				Console.WriteLine();
+				Console.WriteLine();
+
+				//Input Data
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Please, enter the range:");
+				Console.Write(" From: ");
+				from = decimal.TryParse(Console.ReadLine(), out decimal fromValue) ? (int)Math.Round(fromValue) : 0;
+				Console.Write(" To: ");
+				to = decimal.TryParse(Console.ReadLine(), out decimal toValue) ? (int)Math.Round(toValue) : 0;
+				Console.WriteLine();
+				Console.WriteLine();
+
+				//Create FizzBuzz Message
+				Console.ForegroundColor = ConsoleColor.Yellow;
+				Console.WriteLine($"FIZZ - BUZZ RESULT [{from},{to}]:");
+
+				try
+				{
+					fizzbuzzMessage = fizzBuzz.GetFizzBuzzForRange(from, to);
+				}
+				catch (ArgumentException argumentException)
+				{
+					fizzbuzzMessage = argumentException.Message;
+				}
+
+				//Print the FizzBuzz Message and Continue
+				Console.WriteLine(fizzbuzzMessage);
+				Console.WriteLine();
+				Console.WriteLine();
+
+				Console.ForegroundColor = ConsoleColor.White;
+				Console.Write("Press any key to reset");
+				Console.ReadLine();
+				Console.Clear();
+			}
+		}
+	}
+}
