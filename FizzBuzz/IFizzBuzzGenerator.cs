623c28db94f1e7443e7470a834011b7d9e0d9e6b
reytor9010 <reytor9010@gmail.com>
2021-05-23 14:54:51 -0400

    Update IFizzBuzzGenerator.cs
    Add a new method definition to receive the fizz-buzz range as a string
    Add a property definition to get the fizzbuzz range values as a string like [minRangeValue, maxRangeValue]

--- FizzBuzz/IFizzBuzzGenerator.cs
+++ FizzBuzz/IFizzBuzzGenerator.cs
@@ -2,6 +2,9 @@
 {
 	public interface IFizzBuzzGenerator
 	{
+		string Range { get; }
+
+		string GenerateFizzBuzzForRange(string minRangeValue, string maxRangeValue);
 		string GenerateFizzBuzzForRange(float minRangeValue, float maxRangeValue);
 		string GenerateFizzBuzzForRange(int minRangeValue, int maxRangeValue);
 	}
